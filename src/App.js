6b49df5c8eb3a2a7216117ec8198c2e0fbafbdb9
gtech29 <juan.rodriguez.517@my.csun.edu>
2024-05-08 17:12:19 -0700

    update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+// import About from './Components/About Me/About'
+// import Skills from './Components/Skills/Skills'
+// import Projects from './Components/Projects/Projects'
+// import Recommendations from './Components/Recommendations/Recommendations'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      {/* <About />
+      <Skills />
+      <Projects />
+      <Recommendations /> */}
+    </div>
+  )
+}
+
+export default App
